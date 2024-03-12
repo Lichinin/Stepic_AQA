8a1085b846856b70d37b197c4fb6b49f4be8ef0d
Виталий Личинин <lichinin.v@yandex.ru>
2024-03-12 22:35:52 +0300

    add assert

--- test_items.py
+++ test_items.py
@@ -6,4 +6,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_cart(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert browser.find_element(
+        By.CSS_SELECTOR, "button.btn-add-to-basket"
+    ), 'Элемент не найден'
